a3b20774bc7865eabb4c6d736eb9396aad1dc4f4
ladeirass <ladeira@tutanota.com>
2022-09-07 10:09:18 -0300

    meu primeiro commit

---
+++ Algoritmo-1Exercicio/src/aulas/PrimeiroExercicio.java
@@ -0,0 +1,30 @@
+package aulas;
+
+public class PrimeiroExercicio {
+
+	public static void main(String[] args) {
+
+		int numero1 = 10;
+		int numero2 = 15;
+		int idade = 29;
+		double dinheiro = 15.99;
+		
+		String nome = "Felipe";
+		char sexo = 'M';
+		boolean verdadeiro = true;
+		boolean falso = false;
+		
+		System.out.println(numero1);
+		System.out.println(numero2);
+		
+		System.out.println(nome);
+		System.out.println(sexo);
+		
+		System.out.println(verdadeiro);
+		System.out.println(falso);
+		
+		System.out.println(nome + " tem " + idade + "anos, e tem " + dinheiro + " reais");
+		
+	}
+
+}
